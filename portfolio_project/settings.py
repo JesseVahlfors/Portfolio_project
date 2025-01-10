3f56af7f09e437cd4c93f8eb686b28f8842ffb16
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-01-10 20:26:36 +0200

    Added minor setup changes

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +52,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000',
+    'http://127.0.0.1:9000',
 ]
 
 ROOT_URLCONF = 'portfolio_project.urls'
