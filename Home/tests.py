7548a2f7b97dfc9259a987fe7e11bf6c7c2a09eb
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-01-10 20:27:33 +0200

    Add first test to assert "Welcome to my site" is on screen

--- Home/tests.py
+++ Home/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomePageTest(TestCase):
+    def test_home_page_returns_200(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Welcome to my site")
