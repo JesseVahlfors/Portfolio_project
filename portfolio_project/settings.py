3d52b8bb2ce236448b8081ff5c0c0a7c7ead7da1
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-01-26 13:49:06 +0200

    Add GZip middleware for improved performance; adjust image and footer styles for better layout and readability

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
