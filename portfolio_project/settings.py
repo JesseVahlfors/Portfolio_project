05d5d12e7f0a737ddc13e1dd6c4ef3ab7826897a
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-01-28 21:12:51 +0200

    Add AWS S3 settings for media file storage and update MEDIA_URL configuration

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -176,6 +176,27 @@ MEDIA_ROOT = "/media"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+#storage for media files
+
+# AWS S3 settings
+AWS_ACCESS_KEY_ID = os.getenv('B2_APPLICATION_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('B2_APPLICATION_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('B2_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('B2_REGION_NAME', 'us-west-2')  # Default region
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_DEFAULT_ACL = None  # Ensure files are private by default
+AWS_S3_ENDPOINT_URL = f'https://s3.{AWS_S3_REGION_NAME}.backblazeb2.com'
+
+# Media files
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Additional settings
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_QUERYSTRING_AUTH = True  # Set to False if your S3 bucket is public
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
