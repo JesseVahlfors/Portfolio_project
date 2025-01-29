3ffde1f862de9a7ac9f19c9f41efcaec5602daad
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-01-29 21:00:42 +0200

    Update logging configuration in settings.py to use a relative path for file uploads log

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -258,7 +258,7 @@ LOGGING = {
         "file": {
             "level": "DEBUG",
             "class": "logging.FileHandler",
-            "filename": BASE_DIR / "logs/file_uploads.log",
+            "filename": os.path.join(BASE_DIR, "logs/file_uploads.log"),
             "formatter": "detailed",
         },
     },
