3fef08d447dde2a682156801fced4d71a3057109
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-02-02 10:39:30 +0200

    Enhance dropdown functionality and improve styling with new CSS classes for better layout

--- staticfiles/js/dropdown.4d1c15205c47.js
+++ staticfiles/js/dropdown.4d1c15205c47.js
@@ -1,18 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     const dropdownButton = document.getElementById("menu-button");
+    const closeButton = document.getElementById("close-button");
     const dropdownContent = document.getElementById("menu-content");
     const menuItems = dropdownContent.querySelectorAll("a");
 
-    dropdownButton.addEventListener('click', () => {
+    const toggleDropdown = () => {
         dropdownContent.classList.toggle('hidden');
+        dropdownButton.classList.toggle('hidden');
+        closeButton.classList.toggle('hidden');
 
         const isExpanded = dropdownContent.classList.contains("hidden");
         dropdownButton.setAttribute("aria-expanded", !isExpanded);
-    });
+        closeButton.setAttribute("aria-expanded", isExpanded);
+    };
+
+    dropdownButton.addEventListener('click', toggleDropdown);
+    closeButton.addEventListener('click', toggleDropdown);
 
     menuItems.forEach(item => {
         item.addEventListener('click', () => {
             dropdownContent.classList.add('hidden');
+            dropdownButton.setAttribute("aria-expanded", false);
+            closeButton.classList.add('hidden');
             dropdownButton.setAttribute("aria-expanded", false);
         });
     });
