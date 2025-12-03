12ec494dda7cbcee05e665517e5dba71d54196df
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-12-03 08:06:01 +0200

    Add compression_demo app with initial files and update requirements

--- json_parser/urls.py
+++ json_parser/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ParserDemoView.as_view(), name='json_parser'),
-    path('parse/', views.ParseJSONAjaxView.as_view(), name='parse_json'),
+    path('', views.demo_page, name='compression_demo'),
+    path('compress/', views.compress_action, name='compress_action'),
+    path("decompress/", views.decompress_action, name="decompress_action"),
 ]
