937f75a00a4a8cde7621c7d3baaffc929349662c
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-12-18 22:02:21 +0200

    Refactor URL patterns in json_parser: update routes for ParserDemoView and ParseJSONAjaxView

--- json_parser/urls.py
+++ json_parser/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.demo_page, name='compression_demo'),
-    path('compress/', views.compress_action, name='compress_action'),
-    path("decompress/", views.decompress_action, name="decompress_action"),
+    path('', views.ParserDemoView.as_view(), name='json_parser'),
+    path('parse/', views.ParseJSONAjaxView.as_view(), name='parse_json'),
 ]
