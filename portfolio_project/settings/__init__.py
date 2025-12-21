a5a03b0dfe733db68d0ecc9905e97fe28284892e
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2025-12-21 20:28:51 +0200

    Remove unused settings package and related documentation

--- portfolio_project/settings/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Settings package.
-
-Importing ``portfolio_project.settings`` will load development settings by
-default. For production deployments set the environment variable
-``DJANGO_SETTINGS_MODULE`` to ``portfolio_project.settings.prod``.
-"""
-from .dev import *  # noqa: F401,F403
