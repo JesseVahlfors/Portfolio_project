87122a8504333b2bf4d195e8051abe77402dce06
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2026-09-11 14:14:53 +0300

    Implement bubble sort algorithm in BubbleSortView and update URL routing

--- sorting_api/urls.py
+++ sorting_api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('bubble-sort/', BubbleSortView.as_view(), name='bubble-sort'),
+    path('bubble-sort/', views.BubbleSortView.as_view(), name='bubble-sort'),
 ]
--- sorting_api/views.py
+++ sorting_api/views.py
@@ -1,10 +1,34 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 class BubbleSortView(APIView):
     def post(self, request):
-        # read array from request.data
-        # run sorting logic
-        # return JSON
-        return Response(...)
+        inputData = request.data["array"]
+        n = len(inputData)
+        steps = []
+
+        for i in range(n - 1):
+            for j in range(n - i - 1):
+
+                steps.append({
+                    "type": "compare",
+                    "indices": [j, j + 1]
+                })
+                
+
+                if inputData[j] > inputData[j + 1]:
+                    inputData[j], inputData[j + 1] =  inputData[j + 1], inputData[j]
+
+                    steps.append({
+                        "type": "swap",
+                        "indices": [ j, j + 1]
+                    })
+
+
+        responseJson = {
+            "sorted": inputData,
+            "steps": steps
+        }
+
+        return Response(responseJson, status = status.HTTP_200_OK)
